32e569b899a74e20227c4881944d9a247d2703dd
LavieMaru <laviemaru@gmail.com>
2016-10-31 12:37:25 -0400

    Add interview questions

---
+++ interview-Q-hash-separate-chain.js
@@ -0,0 +1,5 @@
+/*Write a hash map implementation which uses separate chaining.*/
+
+function hashChain(argument) {
+	// body...
+}
--- interview-Q-palindrome.js
+++ interview-Q-palindrome.js
@@ -1,6 +1,18 @@
 /*Write an algorithm to check whether any permutation of a string is a palindrome (a string which reads the same forwards and backwards). For example, "madam", "amadm" and "cllci" should all return true, whereas "caabl" and "aaxxis" should return false.*/
 
+function checkPalindrome(str) {
+	
+};
+
 /*Write an algorithm to group a list of words into anagrams. For example, if the input was ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'], the output should be: [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']].*/
+
+function checkAnagram(arr) {
+	//body...
+}
 
 
 /*Write a hash map implementation which uses separate chaining.*/
+
+function hashChain(argument) {
+	// body...
+}
